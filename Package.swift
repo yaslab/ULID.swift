53fdd74e39071e758a1a6a8e5cf3089a6672e844
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2019-01-11 00:25:44 +0900

    Rename

--- Package.swift
+++ Package.swift
@@ -6,23 +6,10 @@ import PackageDescription
 let package = Package(
     name: "ULID.swift",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ULID.swift",
-            targets: ["ULID.swift"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "ULID", targets: ["ULID"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ULID.swift",
-            dependencies: []),
-        .testTarget(
-            name: "ULID.swiftTests",
-            dependencies: ["ULID.swift"]),
+        .target(name: "ULID"),
+        .testTarget(name: "ULIDTests", dependencies: ["ULID"])
     ]
 )
--- Tests/ULIDTests/ULIDTests.swift
+++ Tests/ULIDTests/ULIDTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import ULID_swift
+@testable import ULID
 
 final class ULID_swiftTests: XCTestCase {
     func testExample() {
